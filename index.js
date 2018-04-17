5e0111bafcce20b70b1b8972c4f36636c296acc5
Roigrosa <roigrosa88@gmail.com>
2018-04-17 06:13:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function theBeatlesPlay(musicians, instruments) {
 }
   return array;
 }
+
+function johnLennonFacts(facts) {
+ var results = [];
+ var count = 0;
+ while (count < facts.length) {
+   results.push(facts[count] + "!!!");
+    count++;
+}
+  return results;
+}
